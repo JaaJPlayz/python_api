7ea2de34f3f5432b7e04d353e137c4a7cae68e35
JaaJPlayz <hiagoaugustofcunha@gmail.com>
2023-12-05 13:33:37 -0300

    Progress

--- src/main.py
+++ src/main.py
@@ -4,10 +4,8 @@ from rich.table import Table
 
 console = Console()
 
-# Open a connection to the database
 connection = sqlite3.connect('./src/db/database.sqlite')
 
-# Create a cursor
 cursor = connection.cursor()
 
 
@@ -22,6 +20,13 @@ def insert(item, quantity, price):
     connection.commit()
 
 
+def insert_from_input():
+    item = input("Enter item: ")
+    quantity = int(input("Enter quantity: "))
+    price = float(input("Enter price: "))
+    insert(item, quantity, price)
+
+
 def insert_many(items):
     cursor.executemany("INSERT INTO store VALUES (?, ?, ?)", items)
     connection.commit()
@@ -33,14 +38,14 @@ def view():
     return rows
 
 
-def delete(item):
-    cursor.execute("DELETE FROM store WHERE item=?", (item,))
-    connection.commit()
-
-
 def update(quantity, price, item):
     cursor.execute(
         "UPDATE store SET quantity=?, price=? WHERE item=?", (quantity, price, item))
+    connection.commit()
+
+
+def delete(item):
+    cursor.execute("DELETE FROM store WHERE item=?", (item,))
     connection.commit()
 
 
@@ -52,6 +57,37 @@ def get_formatted_products():
     console.print(productsTable)
 
 
+def application_menu():
+    options = ["Insert", "View", "Update", "Delete", "Exit"]
+    while True:
+        for index, option in enumerate(options):
+            console.print(f"{index+1}. {option}")
+
+        user_input = int(input("Choose an option: "))
+
+        if user_input == 1:
+            insert_from_input()
+
+        elif user_input == 2:
+            get_formatted_products()
+
+        elif user_input == 3:
+            item = input("Enter item to update: ")
+            quantity = int(input("Enter new quantity: "))
+            price = float(input("Enter new price: "))
+            update(quantity, price, item)
+
+        elif user_input == 4:
+            item = input("Enter item to delete: ")
+            delete(item)
+
+        elif user_input == 5:
+            break
+
+        else:
+            console.print("Invalid option", style="bold red")
+
+
 if __name__ == '__main__':
-    get_formatted_products()
+    application_menu()
     connection.close()
